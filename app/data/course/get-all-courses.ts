6ee8825a50848fa13c548cb2327982cee602fb46
Kayque Goldner <kayquegoldner@gmail.com>
2025-09-08 15:38:17 -0300

    add public course listing with cards and loading skeleton

---
+++ app/data/course/get-all-courses.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { prisma } from "@/lib/db";
+
+export async function getAllCourses() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const data = await prisma.course.findMany({
+    where: {
+      status: "Published",
+    },
+    select: {
+      title: true,
+      price: true,
+      smallDescription: true,
+      fileKey: true,
+      id: true,
+      slug: true,
+      level: true,
+      duration: true,
+      category: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
+
+export type GetAllCourses = Awaited<ReturnType<typeof getAllCourses>>;
