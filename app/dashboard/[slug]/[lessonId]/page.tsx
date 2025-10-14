d4849380e8533b92acbdea5fdf1966b1612ad1f0
Kayque Goldner <kayquegoldner@gmail.com>
2025-10-14 13:20:50 -0300

    add lesson skeleton and course progress

--- app/dashboard/[slug]/[lessonId]/page.tsx
+++ app/dashboard/[slug]/[lessonId]/page.tsx
@@ -1,6 +1,9 @@
+import { Suspense } from "react";
+
 import { getLessonContent } from "@/app/data/course/get-lesson-content";
 
 import { CourseContent } from "../[lessonId]/_components/course-content";
+import { LessonSkeleton } from "../[lessonId]/_components/lesson-skeleton";
 
 interface CourseSlugLessonIdPageProps {
   params: Promise<{ lessonId: string }>;
@@ -10,9 +13,18 @@ const CourseSlugLessonIdPage = async ({
   params,
 }: CourseSlugLessonIdPageProps) => {
   const { lessonId } = await params;
-  const data = await getLessonContent(lessonId);
 
-  return <CourseContent data={data} />;
+  return (
+    <Suspense fallback={<LessonSkeleton />}>
+      <LessonContentLoader lessonId={lessonId} />
+    </Suspense>
+  );
 };
 
 export default CourseSlugLessonIdPage;
+
+async function LessonContentLoader({ lessonId }: { lessonId: string }) {
+  const data = await getLessonContent(lessonId);
+
+  return <CourseContent data={data} />;
+}
